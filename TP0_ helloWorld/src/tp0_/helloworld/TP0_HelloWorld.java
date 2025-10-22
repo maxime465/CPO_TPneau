3c597102264b69598df70cb2efa6866655c18ac8
maxneau <maxneau@10.3.62.186>
2025-10-22 15:31:08 +0200

    ajout de projet HW

---
+++ TP0_ helloWorld/src/tp0_/helloworld/TP0_HelloWorld.java
@@ -0,0 +1,29 @@
+/*
+* TP0_ helloWorld 20 octobre 
+ */
+package tp0_.helloworld;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author maxneau
+ */
+public class TP0_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 20; 
+        System.out.println( " Bonjour " );
+        String prenom;
+Scanner sc ;
+sc = new Scanner (System.in) ;
+System.out.println("Quel est votre prénom ?");
+prenom = sc.nextLine();
+        System.out.println( " aurevoir " );
+        // TODO code application logic here
+    }
+    
+}
