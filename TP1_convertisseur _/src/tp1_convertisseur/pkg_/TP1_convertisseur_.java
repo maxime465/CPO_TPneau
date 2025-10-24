c4c1b3af8cd2d57bf06118bab5467c095abd29db
maxneau <maxneau@10.3.62.186>
2025-10-24 11:02:45 +0200

    ajout de projet NI

--- TP1_convertisseur _/src/tp1_convertisseur/pkg_/TP1_convertisseur_.java
+++ TP1_convertisseur _/src/tp1_convertisseur/pkg_/TP1_convertisseur_.java
@@ -12,13 +12,10 @@ public class TP1_convertisseur_ {
     public static void main(String[] args) {        // Demander à l'utilisateur de saisir une valeur réelle
         try ( // Création du Scanner pour lire l'entrée utilisateur
                 Scanner sc = new Scanner(System.in)) {
-            // Demander à l'utilisateur de saisir une valeur réelle
-            System.out.print("Veuillez saisir une valeur réelle : ");
-            // Lire la valeur réelle et la stocker dans une variable de type double
-            double valeur = sc.nextDouble();
-            // Afficher la valeur saisie
-            System.out.println("Vous avez saisi : " + valeur);
-            // Fermeture du scanner
+            System.out.print("Veuillez indiquer un degres : ");
+            double celsius = sc.nextDouble();
+            double kelvin = celsius + 273.15;
+            System.out.println("Vous avez saisi : " + kelvin);
         }
     }
 }
