e78c46feb3f095bacf1ab843215033ca8a357816
maxneau <maxneau@10.3.62.186>
2025-10-24 10:06:37 +0200

    ajout de projet NI

--- TP1_manipNombresInt_/src/tp1_manipnombresint_/TP1_manipNombresInt_.java
+++ TP1_manipNombresInt_/src/tp1_manipnombresint_/TP1_manipNombresInt_.java
@@ -18,23 +18,28 @@ public class TP1_manipNombresInt_ {
         // TODO code application logic here
     
         Scanner sc = new Scanner(System.in);
-
-        // Demande et lecture du premier entier
+        
+     
         System.out.print("saisir entier1: ");
         int a = sc.nextInt();
 
-        // Demande et lecture du deuxième entier
+       
         System.out.print("saisir entier2 : ");
         int b = sc.nextInt();
 
-        // Affichage des valeurs saisies
+
         System.out.println("Vous avez saisi : " + a + " et " + b);
-         int somme = a + b;
+        int somme = a + b;
         int difference = a - b;
         int produit = a * b;
 
      System.out.println("La somme des deux nombres est : " + somme);
         System.out.println("La différence (a - b) est : " + difference);
-        System.out.println("Le produit des deux nombres est : " + produit);}
+        System.out.println("Le produit des deux nombres est : " + produit);
+    int quotient = a / b;
+    int euclidienne = a % b;
+    System.out.println("Le quotient est :" + quotient);
+    System.out.println("la division euclidienne est : " + euclidienne);
+    }
     
 }
