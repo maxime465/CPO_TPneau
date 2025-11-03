bfb1fe62b8d3316bec15c5508f01351635d83dbd
maxneau <maxneau@10.3.62.186>
2025-11-03 15:28:34 +0100

    TP2_Bieres

--- TP2_Bieres/src/tp2_bieres/TP2_Bieres.java
+++ TP2_Bieres/src/tp2_bieres/TP2_Bieres.java
@@ -9,17 +9,12 @@ package tp2_bieres;
  */
 public class TP2_Bieres {
   
-   public class TP2_Bieres_NOM1_NOM2 {
     public static void main(String[] args) {
-        // Création de la première bière
+       
         BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson");
         uneBiere.lireEtiquette();
-
-        // Création de la deuxième bière
         BouteilleBiere deuxiemeBiere = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe");
         deuxiemeBiere.lireEtiquette();
-
-        // Tester l'ouverture d'une bière
         uneBiere.ouvrir();
         uneBiere.lireEtiquette();
 
@@ -27,7 +22,7 @@ public class TP2_Bieres {
         deuxiemeBiere.lireEtiquette();
     }
 }
- }
+ 
 
 
     
