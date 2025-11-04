d0b5510f7c2de126004cb226a3fcd7d07a1663d3
maxneau <maxneau@10.3.62.186>
2025-11-04 17:10:23 +0100

    TP2_Bieres

---
+++ TP2Un convertisseur de températures/src/Convertisseur.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+/*
+ * Convertisseur
+ */
+
+/**
+ *
+ * @author maxneau
+ */
+public class Convertisseur {
+    Scanner sc = new Scanner(System.in)) {
+            System.out.print("Veuillez indiquer un degres : ");
+            double celsius = sc.nextDouble();
+            double kelvin = celsius + 273.15;
+            System.out.println("Vous avez saisi : " + kelvin);
+        }
+}
---
+++ TP2Un convertisseur de températures/src/tp2un/convertisseur/de/températures/TP2UnConvertisseurDeTempératures.java
@@ -0,0 +1,19 @@
+/*
+ * Un convertisseur de températuresis template
+ */
+package tp2un.convertisseur.de.températures;
+
+/**
+ *
+ * @author maxneau
+ */
+public class TP2UnConvertisseurDeTempératures {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
