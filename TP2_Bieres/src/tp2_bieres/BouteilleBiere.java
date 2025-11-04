1157aed1e8475e2a6bf33fba05d3ea2147db97d2
maxneau <maxneau@10.3.62.186>
2025-11-04 09:53:10 +0100

    TP2_Bieres

--- TP2_Bieres/src/tp2_bieres/BouteilleBiere.java
+++ TP2_Bieres/src/tp2_bieres/BouteilleBiere.java
@@ -10,37 +10,34 @@ package tp2_bieres;
  */
 
 public class BouteilleBiere {
-    // Attributs
     String nom;
     double degreAlcool;
     String brasserie;
     boolean ouverte;
 
-    // Constructeur avec paramètres
+  
     public BouteilleBiere(String nom, double degreAlcool, String brasserie) {
         this.nom = nom;
         this.degreAlcool = degreAlcool;
         this.brasserie = brasserie;
-        this.ouverte = false; // par défaut, la bière n'est pas ouverte
-    }
-
-    // Méthode pour lire l'étiquette
-    public void lireEtiquette() {
+        this.ouverte = false;
+}
+     void lireEtiquette() {
         System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degrés) \nBrasserie : " + brasserie);
         System.out.println("Ouverte : " + (ouverte ? "oui" : "non"));
         System.out.println("-----------------------------");
     }
 
-    // Méthode pour ouvrir la bouteille
-    public void ouvrir() {
+    void ouvrir() {
         if (!ouverte) {
             ouverte = true;
             System.out.println(nom + " est maintenant ouverte !");
         } else {
             System.out.println(nom + " est déjà ouverte.");
-        }
-    }
-}
+                }
+                  }
+     }
+
 
    
     
