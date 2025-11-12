b16f1647f4480020aeeeb4084b9500636ad7112a
maxneau <maxneau@10.3.62.186>
2025-11-12 12:27:11 +0100

    TP2_relation

---
+++ TP3_1.1La_classe_Arme/src/main/java/com/mycompany/tp3_1/la_classe_arme/TP3_11La_classe_Arme.java
@@ -0,0 +1,16 @@
+/*
+ * TP3_1.1 La classe Arme
+ */
+
+package com.mycompany.tp3_1.la_classe_arme;
+
+/**
+ *
+ * @author maxneau
+ */
+public class TP3_11La_classe_Arme {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
