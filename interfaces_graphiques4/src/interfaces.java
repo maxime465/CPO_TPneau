9f9a059bcb6e4e947103be915ded87cc468ab7fc
maxneau <maxneau@10.3.62.186>
2025-11-26 17:09:52 +0100

    TPinterfaces

--- interfaces_graphiques4/src/interfaces.java
+++ interfaces_graphiques4/src/interfaces.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-
+   
 /**
  *
  * @author maxneau
@@ -17,13 +17,9 @@ int compteur=0 ;
      */
     public interfaces() {
         initComponents();
-  
+
         
-        
-        
-        
-        
-    }
+      }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -36,21 +32,58 @@ int compteur=0 ;
 
         zoneTexte = new javax.swing.JLabel();
         Bouton = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         zoneTexte.setFont(new java.awt.Font("Herculanum", 2, 36)); // NOI18N
-        zoneTexte.setText("BONJOUR");
-        getContentPane().add(zoneTexte, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 120, -1, -1));
+        zoneTexte.setText("Question1");
+        getContentPane().add(zoneTexte, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 20, 180, 40));
 
-        Bouton.setText("OKAYYYY");
+        Bouton.setText("1");
         Bouton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BoutonActionPerformed(evt);
             }
         });
-        getContentPane().add(Bouton, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 240, -1, -1));
+        getContentPane().add(Bouton, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 110, 70, 50));
+
+        jButton1.setText("2");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 110, 70, 50));
+
+        jButton2.setText("3");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 110, 70, 50));
+
+        jButton3.setText("4");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 110, 70, 50));
+
+        jButton4.setFont(new java.awt.Font("Herculanum", 0, 13)); // NOI18N
+        jButton4.setText("Next question");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 260, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -62,6 +95,24 @@ int compteur=0 ;
          if (compteur==2) zoneTexte.setText ("Du kombucha ");
 compteur++;
     }//GEN-LAST:event_BoutonActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+     
+
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton4ActionPerformed
 
  
     /**
@@ -91,6 +142,10 @@ compteur++;
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Bouton;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel zoneTexte;
     // End of variables declaration//GEN-END:variables
 }
